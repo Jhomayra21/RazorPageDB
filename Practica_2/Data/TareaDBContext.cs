b3a22580534576a8e51f41afed8e1250c0cd888b
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-08-26 21:01:02 -0400

    conexion

---
+++ Practica_2/Data/TareaDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practica_2.Models;
+
+namespace Practica_2.Data
+{
+    public class TareaDBContext:DbContext
+    {
+        public TareaDBContext(DbContextOptions<TareaDBContext> options):base(options)//investigacion de que hace esta linea
+        {
+
+        }
+        public DbSet<Tarea> Tareas { get; set; }//tarea de investigacion 
+        protected TareaDBContext()
+        {
+
+        }
+    }
+}
---
+++ Practica_2/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace Practica_2.Models
+{
+    public class Tarea
+    {
+        public int idTarea { get; set; }
+        public string nombreTarea { get; set; }
+        public DateTime fechaVencimiento { get; set; }
+        public string estado { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
